fde5e2650c79883c74b0b4e0f9c644d212c96c8f
jdyeakel <jdyeakel@gmail.com>
2021-01-15 19:04:12 -0800

    9 panel fig

--- bodysizefoodwebs_benguela_nw.jl
+++ bodysizefoodwebs_benguela_nw.jl
@@ -30,7 +30,46 @@ Aprime_bg = Matrix(Adata_bg[1:numsp,2:numsp+1]);
 A_bg = copy(Aprime_bg);
 # massvec_bg = massdata[:mass][5:numsp];
 massvec_bg = copy(vec(massdata_bg[!,:mass_kg]));
+#Sort by body size
+sortsp_bg = sortperm(massvec_bg);
+Asort_bg = A_bg[sortsp_bg,sortsp_bg];
 
+#Import Potters Cove and Barrents Sea adjacency matrices
+Adata_pc = CSV.read("$(homedir())/Dropbox/Funding/20_NSF_OCE/oceanfoodwebs_2020/foodweb_model/foodwebdata_raw/Antarctica_PotterCove/adjmatrix.csv",header=true);
+massdata_pc = CSV.read("$(homedir())/Dropbox/Funding/20_NSF_OCE/oceanfoodwebs_2020/foodweb_model/foodwebdata_raw/Antarctica_PotterCove/masslist.csv",header=true);
+## POTTERS COVE
+Aprime_pc = Matrix(Adata_pc[1:88,2:89])
+#Only keep Role >1 (do not have length estimates for Role = 1 ~ primary producers)
+keep = findall(x->x>1,vec(massdata_pc[!,:role]));
+A_pc = copy(Aprime_pc[keep,keep]);
+massvec_pc = massdata_pc[!,:length_cm][keep];
+#delete completely disconnected species
+keep2 = findall(!iszero,vec(sum(A_pc,dims=1)).+vec(sum(A_pc,dims=2)));
+A_pc = A_pc[keep2,keep2];
+massvec_pc = massvec_pc[keep2];
+#Sorting
+sortsp = sortperm(massvec_pc);
+Asort_pc = A_pc[sortsp,sortsp];
+
+
+Adata_bts = CSV.read("$(homedir())/Dropbox/Funding/20_NSF_OCE/oceanfoodwebs_2020/foodweb_model/foodwebdata_raw/BarentsSea/Barents_adjmatrix.csv",header=true);
+massdata_bts = CSV.read("$(homedir())/Dropbox/Funding/20_NSF_OCE/oceanfoodwebs_2020/foodweb_model/foodwebdata_raw/BarentsSea/Barents_masslist.csv",header=true);
+## BARENTS SEA
+# NOTE: NEED TO REMOVE DETRITUS, ETC
+Aprime_bts = Matrix(Adata_bts[1:159,2:160]);
+role_bts = massdata_bts[!,:role];
+#Exclude primary producers (role = 1), baleen whales (role = 4) and birds (role=3)
+keep = findall(x->x>1 && x<3,role_bts);
+A_bts = Aprime_bts[keep,keep];
+massvec_bts = massdata_bts[!,:length_cm][keep];
+namesvec_bts = Adata_bts[!,:species][keep];
+keep2 = findall(x->x>1,massvec_bts);
+A_bts = A_bts[keep2,keep2];
+massvec_bts = massvec_bts[keep2];
+namesvec_bts = namesvec_bts[keep2];
+#Sorting
+sortsp = sortperm(massvec_bts);
+Asort_bts = A_bts[sortsp,sortsp];
 
 #######################
 # FITTING
@@ -44,21 +83,33 @@ results_bg = optimize(x->lfunc(x,A_bg,massvec_bg),x0,NelderMead());
 results_bg.minimizer
 xmax_bg = results_bg.minimizer;
 fcorr_bg, Apredict_bg = fc(xmax_bg,A_bg,massvec_bg)
-
+## POTTERS COVE
 x0 = [0.0,0.0,0.0];
-Anull_bg = nullweb(A_bg);
-resultsnull_bg = optimize(x->lfunc(x,Anull_bg,massvec_bg),x0,NelderMead());
-resultsnull_bg.minimizer
-xmax = resultsnull_bg.minimizer;
-fcorrnull_bg, Apredictnull_bg = fc(xmax,Anull_bg,massvec_bg)
-
-sortsp_bg = sortperm(massvec_bg);
-Asort_bg = A_bg[sortsp_bg,sortsp_bg];
+results_pc = optimize(x->lfunc(x,A_pc,massvec_pc),x0,NelderMead());
+results_pc.minimizer
+xmax_pc = results_pc.minimizer;
+fcorr_pc, Apredict_pc = fc(xmax_pc,A_pc,massvec_pc)
+## BARENTS SEA
+x0 = [0.0,0.0,0.0];
+results_bts = optimize(x->lfunc(x,A_bts,massvec_bts),x0,NelderMead());
+results_bts.minimizer
+xmax_bts = results_bts.minimizer;
+fcorr_bts, Apredict_bts = fc(xmax_bts,A_bts,massvec_bts);
 
 #Sort and isolate consumers only from Apredict
 Apredict_bg_sort = Apredict_bg[sortsp_bg,sortsp_bg];
 cons = findall(x->x>0,vec(sum(Apredict_bg_sort,dims=1)));
 Apredict_bg_sort_cons = Apredict_bg_sort[cons,cons];
+
+# Apredict_bg_sort = Apredict_bg[sortsp_bg,sortsp_bg];
+cons = findall(x->x>0,vec(sum(Apredict_pc,dims=1)));
+Apredict_pc_sort_cons = Apredict_pc[cons,cons];
+
+# Apredict_bg_sort = Apredict_bg[sortsp_bg,sortsp_bg];
+cons = findall(x->x>0,vec(sum(Apredict_bts,dims=1)));
+Apredict_bts_sort_cons = Apredict_bts[cons,cons];
+
+
 
 # use xmax to build a random food web
 
@@ -99,6 +150,11 @@ end
 # Calculate some network metrics
 R"""
 library(UNODF)
+library(igraph)
+library(NetIndices)
+library(fields)
+library(plot.matrix)
+library(RColorBrewer)
 """
 reps = 5000;
 rich = Array{Float64}(undef,reps,2);
@@ -107,6 +163,8 @@ nest = Array{Float64}(undef,reps,2);
 mod = Array{Float64}(undef,reps,2);
 nind2 = Array{Float64}(undef,reps,2);
 nind3 = Array{Float64}(undef,reps,2);
+maxtl = Array{Float64}(undef,reps,2);
+
 
 nind2_1700_sp = Array{Float64}(undef,reps,23);
 nind3_1700_sp = Array{Float64}(undef,reps,23);
@@ -152,6 +210,11 @@ for r=1:reps
     aprime3_1700 = bgmatrix_1700^3;
     nind3_1700 = sum(aprime3_1700);
 
+    #TL
+    R"ctl_1700 <- TrophInd($(bgmatrix_1700))[,1]"
+    @rget ctl_1700;
+    
+
     
     rich[r,1] = rich_1700;
     conn[r,1] = sum(bgmatrix_1700)/size(bgmatrix_1700)[1]^2;
@@ -159,6 +222,7 @@ for r=1:reps
     mod[r,1] = mod_1700;
     nind2[r,1] = nind2_1700;
     nind3[r,1] = nind3_1700;
+    maxtl[r,1] = maximum(ctl_1700);
 
     date = "2000";
     occur = findall(!iszero,nw_bs[!,Symbol(date)]);
@@ -195,12 +259,18 @@ for r=1:reps
     nind2_2000 = sum(aprime2_2000);
     nind3_2000 = sum(aprime3_2000);
 
+    #TL
+    R"ctl_2000 <- TrophInd($(bgmatrix_2000))[,1]"
+    @rget ctl_2000;
+    
+
     rich[r,2] = rich_2000;
     conn[r,2] = sum(bgmatrix_2000)/size(bgmatrix_2000)[1]^2;
     nest[r,2] = nest_2000;
     mod[r,2] = mod_2000;
     nind2[r,2] = nind2_2000;
     nind3[r,2] = nind3_2000;
+    maxtl[r,2] = maximum(ctl_2000);
 
     # Contribution of each species
     #1700
@@ -421,6 +491,8 @@ fullind2_2000 = nind2_2000_sp;
 fullind3_2000 = nind3_2000_sp;
 
 namesboth[findall(x->x>1.5,mspindirect3_2000)]
+apos[findall(x->x>1.5,mspindirect3_2000)]
+
 
 namespace = "$(homedir())/Dropbox/Funding/20_NSF_OCE/oceanfoodwebs_2020/foodweb_model/figures/foodweb_4panelinset_trim_indirect_species.pdf";
 R"""
@@ -442,5 +514,100 @@ points($(mspindirect3_1700[apos]),$(mspindirect3_2000),pch=16,col=pal[3],cex=1.2
 # lines(rep(1,10),seq(0,2,length.out=10),col=pal[4])
 mtext(side=1,expression(paste("Ratio indirect interactions (1700)")),line=2.5)
 mtext(side=2,expression(paste("Ratio indirect interactions (2000)")),line=2)
+dev.off()
+"""
+
+
+
+
+
+# layoutmatrix = reshape([1,1,3,3,6,6,1,1,3,3,6,6,1,1,4,4,7,7,2,2,4,4,7,7,2,2,5,5,8,9,2,2,5,5,8,9],(6,6))
+namespace = "$(homedir())/Dropbox/Funding/20_NSF_OCE/oceanfoodwebs_2020/foodweb_model/figures/foodweb_9panel.pdf";
+R"""
+library(igraph)
+library(fields)
+library(plot.matrix)
+library(RColorBrewer)
+pdf($namespace,width=20,height=20)
+# par(mfrow=c(2,2))
+# layout(($layoutmatrix), widths=rep(1,6*6), heights=rep(1,6*6))
+# par(list(oma = c(3, 3, 3, 3), mar = c(3, 3, 3, 3)))
+par(list(oma = c(3, 3, 0, 0), mar = c(2, 2, 1, 0)))
+
+
+pal = rev(brewer.pal(9,'YlOrRd'))
+pijarray = $pijarray
+rownames(pijarray) = $predmasses
+colnames(pijarray) = $preymasses
+margin = 0.05
+# PLOT 1
+# c(x1, x2, y1, y2)
+par(list(new=TRUE, plt=c(0*(1+0.01), 0.33*(1-0.01), .66*(1+0.01), 1*(1-0.01))))
+image(t(apply(t($Asort_bts), 1, rev)),col=c('white','black'),legend=FALSE,xaxt='n',yaxt='n')
+# axis(1,labels=FALSE,tick=FALSE)
+# axis(2,labels=FALSE,tick=FALSE)
+# axis(3,labels=FALSE,tick=FALSE)
+# axis(4,labels=FALSE,tick=FALSE)
+# axis.col=NULL, axis.row=NULL,
+# PLOT 2
+# c(x1, x2, y1, y2)
+par(list(new=TRUE, plt=c(.33*(1+0.01), 0.66*(1-0.01), .66*(1+0.01), 1*(1-0.01))))
+# plot($Asort_pc,col=c('white','black'),key=NULL, axis.col=NULL, axis.row=NULL,border=NA,main='',xlab='',ylab='')
+image(t(apply(t($Asort_pc), 1, rev)),col=c('white','black'),legend=FALSE,xaxt='n',yaxt='n')
+
+# PLOT 3
+# c(x1, x2, y1, y2)
+par(list(new=TRUE, plt=c(.66*(1+0.0), 1*(1-0.01), .66*(1+0.01), 1*(1-0.01))))
+# plot($Asort_bg,col=c('white','black'),key=NULL, axis.col=NULL, axis.row=NULL,border=NA,main='',xlab='',ylab='')
+image(t(apply(t($Asort_bg), 1, rev)),col=c('white','black'),legend=FALSE,xaxt='n',yaxt='n')
+
+# PLOT 4
+# c(x1, x2, y1, y2)
+par(list(new=TRUE, plt=c(0*(1+margin), 0.5*(1-margin), .33*(1+margin), 0.66*(1-0))))
+image.plot(x=$predmasses,y=$preymasses,z=$pijarray,col=pal,legend.args=list(text='     Pr(link)',
+side=3,
+line=.5), xlab='Pred mass (kg)', ylab = 'Prey mass (kg)',cex.axis=1.5, cex.lab=1.5
+)
+pal = rev(brewer.pal(9,'YlGnBu'))
+
+# PLOT 5
+# c(x1, x2, y1, y2)
+par(list(new=TRUE, plt=c(0.55*(1+margin), 1*(1-margin), .33*(1+margin), 0.66*(1-0))))
+plot($(Apredict_bg_sort_cons),axis.col=1,main='',col=pal,fmt.key="%.2f",xlab='',ylab='',cex.axis=1.5, cex.lab=1.5)
+
+# Plot 6
+# c(x1, x2, y1, y2)
+par(list(new=TRUE, plt=c(0*(1+margin), 0.33*(1-margin), 0*(1+margin), 0.33*(1-margin))))
+# plot($(bgmatrix_1700),axis.col=3,main='',key=NULL)
+pal = colorRampPalette(rev(brewer.pal(11,"Spectral")))(max(c($ctlcolors_1700,$ctlcolors_2000)))
+agraph = graph_from_adjacency_matrix($(bgmatrix_1700))
+lay <- layout.fruchterman.reingold(agraph)
+# lay <- layout_on_sphere(agraph)
+lay[,2] <- ctl_1700
+plot.igraph(agraph,layout = lay,vertex.color=pal[$ctlcolors_1700],vertex.size=18,edge.arrow.size=0.5,vertex.label.color='white',vertex.label=$id1700)
+# legend(0,10,lv,col=pal[lv],pch=16)
+
+# Plot 7
+# c(x1, x2, y1, y2)
+par(list(new=TRUE, plt=c(.33*(1+margin), 0.66*(1-margin), 0*(1+margin), 0.33*(1-margin))))
+# plot($(bgmatrix_2000),axis.col=3,main='',key=NULL)
+# pal = colorRampPalette(rev(brewer.pal(11,"Spectral")))(max($ctlcolors_2000))
+agraph = graph_from_adjacency_matrix($(bgmatrix_2000))
+lay <- layout.fruchterman.reingold(agraph)
+# lay <- layout_on_sphere(agraph)
+lay[,2] <- ctl_2000
+plot.igraph(agraph,layout = lay,vertex.color=pal[$ctlcolors_2000],vertex.size=18,edge.arrow.size=0.5,vertex.label.color='white',vertex.label=$id2000)
+
+pal = brewer.pal(3,'Set1')
+# PLOT 8
+# c(x1, x2, y1, y2)
+par(list(new=TRUE, plt=c(0.66*(1+margin), 1*(1-margin), 0.16*(1+margin), 0.33*(1-margin))))
+boxplot($nind2,names=c('',''),boxwex=0.25,col=pal[2])
+
+# PLOT 9
+# c(x1, x2, y1, y2)
+par(list(new=TRUE, plt=c(0.66*(1+margin), 1*(1-margin), 0*(1+margin), 0.16*(1-margin))))
+boxplot($nind3,names=c('1700','2000'),boxwex=0.25,col=pal[3])
+
 dev.off()
 """
